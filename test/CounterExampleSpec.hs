f8a7d13cc434820d10479dab6deaf172fb51cd60
DinosaurMars <62925825+AbigailMars@users.noreply.github.com>
2021-09-04 20:31:03 +0200

    counterexamples check

---
+++ test/CounterExampleSpec.hs
@@ -0,0 +1,36 @@
+module CounterExampleSpec where
+
+import CounterExample
+import Test
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Control.Monad (void)
+
+spec :: Spec
+spec = do
+  describe "checkConnection" $ void $ sequence
+    [ it ("isSuccessful for " ++ name) $ checkConnection code `shouldBe` Nothing
+    | (name, code) <-
+        [ ("outerStateDiagC1", outerStateDiagC1)
+         ,("outerStateDiagC2 ", outerStateDiagC2)
+         ,("outerStateDiagC3", outerStateDiagC3)
+         ,("insideStateDiagC1", insideStateDiagC1)
+         ,("insideStateDiagC2", insideStateDiagC2)
+         ,("insideStateDiagC3", insideStateDiagC3)
+         ,("outerCombineDiagC1", outerCombineDiagC1)
+         ,("insideCombineDiagC1", insideCombineDiagC1)
+        ]
+    ]
+  describe "checkUniqueness" $ void $ sequence
+    [ it ("isSuccessful for " ++ name) $ checkUniqueness code `shouldBe` Nothing
+    | (name, code) <-
+        [("outerStateDiagL1", outerStateDiagL1)
+        ,("outerStateDiagL2", outerStateDiagL2)
+        ,("outerStateDiagL3", outerStateDiagL3)
+        ,("insideStateDiagL1", insideStateDiagL1)
+        ,("insideStateDiagL2", insideStateDiagL2)
+        ,("insideStateDiagL3", insideStateDiagL3)
+        ,("outerCombineDiagL1", outerCombineDiagL1)
+        ,("insideCombineDiagL1", insideCombineDiagL1)
+        ]
+    ]
