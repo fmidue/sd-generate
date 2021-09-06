1cf7518ea2ddcbaae80c68775f6fe0c211d824a5
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2021-09-06 12:41:35 +0200

    have only one list of positive cases

--- test/ExampleSpec.hs
+++ test/ExampleSpec.hs
@@ -10,103 +10,18 @@ spec :: Spec
 spec = do
   describe "checkConnection" $ void $ sequence
     [ it ("isSuccessful for " ++ name) $ checkConnection code `shouldBe` Nothing
-    | (name, code) <-
-        [ ("slide246", slide246)
-        , ("slide253", slide253)
-        , ("slide257", slide257)
-        , ("slide267a", slide267a)
-        , ("slide267b", slide267b)
-        , ("slide271", slide271)
-        , ("slide273", slide273)
-        , ("slide275", slide275)
-        , ("slide277", slide277)
-        , ("slide278", slide278)
-        , ("slide279", slide279)
-        , ("slide280", slide280)
-        , ("slide281", slide281)
-        , ("slide283", slide283)
-        , ("task26a", task26a)
-        , ("task26b", task26b)
-        , ("task27", task27)
-        , ("task85", task85)
-        , ("task88", task88)
-        , ("test1", test1)
-        , ("test2", test2)
-        , ("test3", test3)
-        , ("test4", test4)
-        , ("picture1", picture1)
-        , ("picture2", picture2)
-        , ("picture3", picture3)
-        , ("picture4", picture4)
-        ]
-    ]
+    | (name, code) <- positiveExamples ]
   describe "checkUniqueness" $ void $ sequence
     [ it ("isSuccessful for " ++ name) $ checkUniqueness code `shouldBe` Nothing
-    | (name, code) <-
-        [ ("slide246", slide246)
-        , ("slide253", slide253)
-        , ("slide257", slide257)
-        , ("slide267a", slide267a)
-        , ("slide267b", slide267b)
-        , ("slide271", slide271)
-        , ("slide273", slide273)
-        , ("slide275", slide275)
-        , ("slide277", slide277)
-        , ("slide278", slide278)
-        , ("slide279", slide279)
-        , ("slide280", slide280)
-        , ("slide281", slide281)
-        , ("slide283", slide283)
-        , ("task26a", task26a)
-        , ("task26b", task26b)
-        , ("task27", task27)
-        , ("task85", task85)
-        , ("task88", task88)
-        , ("test1", test1)
-        , ("test2", test2)
-        , ("test3", test3)
-        , ("test4", test4)
-        , ("picture1", picture1)
-        , ("picture2", picture2)
-        , ("picture3", picture3)
-        , ("picture4", picture4)
-        ]
-    ]
+    | (name, code) <- positiveExamples ]
   describe "checkValidity" $ void $ sequence
     [ it ("isSuccessful for " ++ name) $ checkValidity code `shouldBe` Nothing
-    | (name, code) <-
-        [ ("slide246", slide246)
-        , ("slide253", slide253)
-        , ("slide257", slide257)
-        , ("slide267a", slide267a)
-        , ("slide267b", slide267b)
-        , ("slide271", slide271)
-        , ("slide273", slide273)
-        , ("slide275", slide275)
-        , ("slide277", slide277)
-        , ("slide278", slide278)
-        , ("slide279", slide279)
-        , ("slide280", slide280)
-        , ("slide281", slide281)
-        , ("slide283", slide283)
-        , ("task26a", task26a)
-        , ("task26b", task26b)
-        , ("task27", task27)
-        , ("task85", task85)
-        , ("task88", task88)
-        , ("test1", test1)
-        , ("test2", test2)
-        , ("test3", test3)
-        , ("test4", test4)
-        , ("picture1", picture1)
-        , ("picture2", picture2)
-        , ("picture3", picture3)
-        , ("picture4", picture4)
-        ]
-    ]
+    | (name, code) <- positiveExamples ]
   describe "checkWrapper" $ void $ sequence
     [ it ("isSuccessful for " ++ name) $ checkWrapper code `shouldBe` Nothing
-    | (name, code) <-
+    | (name, code) <- positiveExamples ]
+
+positiveExamples =
         [ ("slide246", slide246)
         , ("slide253", slide253)
         , ("slide257", slide257)
@@ -135,4 +50,3 @@ spec = do
         , ("picture3", picture3)
         , ("picture4", picture4)
         ]
-    ]
