3ba1166970ea8824347361b878158926735e671f
DinosaurMars <62925825+AbigailMars@users.noreply.github.com>
2021-09-16 00:07:53 +0200

    first version of Generate.hs
    nested start state ,connection  still cannot be generated ; and needs adjustments of function name and nested relation of Combination diagrams and StateDiagram

--- src/Generate.hs
+++ src/Generate.hs
@@ -1,6 +1,7 @@
 module Generate where
 import Datatype
-import Test.QuickCheck
+import Test.QuickCheck hiding(label)
+import Test
 
 randomSD :: Gen UMLStateDiagram
 randomSD = do
@@ -8,13 +9,65 @@ randomSD = do
   if bottom
     then
     do
+      n <- elements [2 .. 3]
+      subs <- vectorOf n randomSubstateCD
       l <- elements [1 .. 3]
-      elements [Joint l, History l Shallow, InnerMostState l "" ""]
+      if (any checkListInSD subs) && (isUnique (map label subs))
+        then
+        do
+          return (CombineDiagram subs l)
+        else
+        do
+          randomSD
     else
     do
       n <- elements [1 .. 2]
-      subs <- vectorOf n randomSD
+      subs <- vectorOf n randomInner
       l <- elements [1 .. 3]
       nm <- elements ["A","B","C"]
-      start <- elements [1 .. n]
-      return (StateDiagram subs l nm [] [start])
+      start <- elements (map label subs)
+      if (any checkListInSD subs) && (isUnique (map label subs))
+        then
+        do
+          return (StateDiagram subs l nm [] [start])
+        else
+        do
+          randomSD
+
+randomSubstateCD :: Gen UMLStateDiagram
+randomSubstateCD = do
+      n <- elements [1 .. 2]
+      subs <- vectorOf n randomInner
+      l <- elements [1 .. 3]
+      nm <- elements ["A","B","C"]
+      start <- elements (map label subs)
+      if (any checkListInSD subs) && (isUnique (map label subs ))
+        then
+        do
+          return (StateDiagram subs l nm [] [start])
+        else
+        do
+          randomSubstateCD
+
+randomInner :: Gen UMLStateDiagram
+randomInner = do
+  bottom <- elements [False, True]
+  if bottom
+    then
+    do
+      l <- elements [1 .. 4]
+      elements [Joint l, History l Shallow,InnerMostState l "" "",History l Deep]
+    else
+    do
+      n <- elements [1 .. 2]
+      subs <- vectorOf n randomInner
+      l <- elements [1 .. 3]
+      nm <- elements ["A","B","C"]
+      start <- elements (map label subs)
+      if (any checkListInSD subs) && (isUnique (map label subs))
+        then
+        do
+          return (StateDiagram subs l nm [] [start])
+        else
+        do
+          randomInner
