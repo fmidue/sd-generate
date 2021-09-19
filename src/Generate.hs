c33162076827c5a09b78a327c0ea3f7b85cd278f
DinosaurMars <62925825+AbigailMars@users.noreply.github.com>
2021-09-19 02:44:35 +0200

    add name parameter to InnerMostState

--- src/Generate.hs
+++ src/Generate.hs
@@ -26,8 +26,9 @@ randomInnerSD c l =
             randomInnerMost l
 
 randomInnerMost :: Int -> Gen UMLStateDiagram
-randomInnerMost l =
-       frequency [(2,return (Joint l)),(1,return (History l Shallow)),(1,return (History l Deep)),(10,return (InnerMostState l "" ""))]
+randomInnerMost l = do
+       nm <- return (last (take l ["A","B","C","D","E"]))
+       frequency [(2,return (Joint l)),(1,return (History l Shallow)),(1,return (History l Deep)),(10,return (InnerMostState l nm ""))]
 
 randomCD :: Int -> Int -> Gen UMLStateDiagram
 randomCD c l = do
@@ -40,7 +41,7 @@ randomCD c l = do
 randomSubstateCD :: Int -> Int -> Gen UMLStateDiagram
 randomSubstateCD c l = do
       let counter = c-1
-      n <- elements [3 .. 5]
+      n <- elements [2 .. 3]
       labels <- shuffle [1..n]
       subs <- mapM (randomInnerSD counter) labels `suchThat` any checkListInSD
       nm <- elements ["A","B","C"]
