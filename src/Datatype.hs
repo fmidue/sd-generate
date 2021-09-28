3677be876fd3c3e8a32fc22e97af55dd6a5ba472
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2021-09-29 00:45:02 +0200

    add normalisation of state diagrams (fmidue/ba-le-zhang#15)
    - normalise state diagrams before drawing
    - close fmidue/ba-le-zhang#15

--- src/Datatype.hs
+++ src/Datatype.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE TypeFamilies              #-}
 module Datatype where
 
+import Data.List                        (partition)
+
 data Wrapper =
   OrDecom { layered :: [[Wrapper]],
             key :: Int,
@@ -104,3 +106,36 @@ data UMLStateDiagram = StateDiagram { substate :: [UMLStateDiagram],
 data Layout = Vertical | Horizontal | Unspecified deriving (Show, Eq)
 
 data RightConnect = WithArrowhead | WithoutArrowhead | NoConnection deriving (Show, Eq)
+
+localiseConnection :: Connection -> ([Int], Connection)
+localiseConnection c = commonPrefix (pointFrom c) (pointTo c)
+  where
+    commonPrefix []  _  = error "connection has no source"
+    commonPrefix _   [] = error "connection has no target"
+    commonPrefix [x] ys = ([], c { pointFrom = [x], pointTo = ys })
+    commonPrefix xs [y] = ([], c { pointFrom = xs, pointTo = [y] })
+    commonPrefix (x:xs) (y:ys)
+      | x == y
+      = let (cps, c') = commonPrefix xs ys
+        in (x:cps, c')
+      | otherwise
+      = ([], c { pointFrom = x:xs, pointTo = y:ys })
+
+localise :: UMLStateDiagram -> UMLStateDiagram
+localise = localiseStateDiagram []
+
+localiseStateDiagram :: [([Int], Connection)] -> UMLStateDiagram -> UMLStateDiagram
+localiseStateDiagram cs s = case s of
+  StateDiagram {}   -> s {
+    connection = map snd local,
+    substate   = subs
+    }
+  CombineDiagram {} -> s {
+    substate   = subs
+    }
+  _                 -> s
+  where
+    matching = [ (cps, c) | (cp:cps, c) <- cs, label s == cp]
+    (local, global) = partition (null . fst)
+      $ matching ++ map localiseConnection (connection s)
+    subs     = map (localiseStateDiagram global) $ substate s
