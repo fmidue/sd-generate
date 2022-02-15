bd4490a9cfc018e959d204d0b0d1713115918969
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-02-15 09:14:44 +0100

    some more checking

--- test/GenerateSpec.hs
+++ test/GenerateSpec.hs
@@ -1,6 +1,7 @@
 module GenerateSpec (spec) where
 
 import Generate
+import Datatype (localise, globalise)
 import ExampleSpec (allTheCheckers)
 
 import Test.QuickCheck
@@ -14,6 +15,12 @@ spec = do
   describe "randomSD" $
     prop "generates valid diagram expressions" $
       forAll randomSD $ \(code,_) -> firstJust id (map (($ code) . snd) allTheCheckers) `shouldBe` Nothing
+  describe "randomSD" $
+    prop "generates valid diagram expressions after 'localise'" $
+      forAll randomSD $ \(code,_) -> firstJust id (map (($ localise code) . snd) allTheCheckers) `shouldBe` Nothing
+  describe "randomSD" $
+    prop "generates valid diagram expressions after 'globalise'" $
+      forAll randomSD $ \(code,_) -> firstJust id (map (($ globalise code) . snd) allTheCheckers) `shouldBe` Nothing
   describe "randomSD" $
     prop "doesn't retry too often" $
       forAll randomSD $ \(_,attempts) -> attempts `shouldSatisfy` (< 90)
