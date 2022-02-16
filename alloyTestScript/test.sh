7356733125a9d7b68f189106c731d5c54e7981be
Zixin Wu <93346565+guard-who@users.noreply.github.com>
2022-02-16 13:24:30 +0100

    Alloy test script that I used.

---
+++ alloyTestScript/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+java -jar AlloyCommandline.jar F:/Germany/Bachelor_Thesis/Github/ba-zixin-wu/alloy/sd/version_4/picture1.als
+java -jar AlloyCommandline.jar F:/Germany/Bachelor_Thesis/Github/ba-zixin-wu/alloy/sd/version_4/concrete_example2.als
+java -jar AlloyCommandline.jar F:/Germany/Bachelor_Thesis/Github/ba-zixin-wu/alloy/sd/version_4/uml_state_diagram.als
+java -jar AlloyCommandline.jar F:/Germany/Bachelor_Thesis/Github/ba-zixin-wu/alloy/sd/version_0/uml_state_diagram_basic_structure.als
+java -jar AlloyCommandline.jar F:/Germany/Bachelor_Thesis/Github/ba-zixin-wu/alloy/sd/version_1/uml_state_diagram_version1.als
+java -jar AlloyCommandline.jar F:/Germany/Bachelor_Thesis/Github/ba-zixin-wu/alloy/sd/version_2/uml_state_diagram_version2.als
