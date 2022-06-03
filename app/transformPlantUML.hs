3548d5450367b0f8aa0a1e2e0d2826515ea60895
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-06-03 12:03:56 +0200

    automatic rendering using call-plantuml

--- app/transformPlantUML.hs
+++ app/transformPlantUML.hs
@@ -4,6 +4,14 @@ import PlantUMLDiagrams(renderAll)
 import Example (positiveExamples)
 import Control.Monad (forM_)
 
+import Prelude hiding (writeFile)
+import Data.ByteString.Char8 (pack, writeFile)
+import Language.PlantUML.Call (drawPlantUMLDiagram, DiagramType(..))
+
 main :: IO ()
 main = forM_ positiveExamples $
-  \(file, sd) -> writeFile (file ++ ".txt") $ renderAll sd
+  \(file, sd) -> do
+    let plantUML = pack (renderAll sd)
+    writeFile (file ++ ".txt") plantUML
+    picture <- drawPlantUMLDiagram SVG plantUML
+    writeFile (file ++ ".svg") picture
