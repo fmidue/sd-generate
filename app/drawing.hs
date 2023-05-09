5ab8c26a8e32d6172887ebdc293148bc6f0e8c0f
sjselest <sergei.lestsenski@stud.uni-due.de>
2023-05-09 21:05:00 +0200

    wip flattening strategy
    raw approach of transformation, by cp'ing states of the diagram. transitions are not handled yet.

--- app/drawing.hs
+++ app/drawing.hs
@@ -2,18 +2,19 @@ import Diagrams.Backend.SVG.CmdLine
 import Example
 import Layout (drawDiagram)
 import Example (testFlat1Combine
+               ,testFlat1Input
                ,testFlatConReg1
                ,testFlatConReg2)
 import Datatype ( localise
                   ,globalise )
-import Flatten (crossInms
-               ,unionTrns
-               ,mrgInmsCP)
+import Flatten (flatten
+               ,getLabelStr)
 
 main = do
-  print (unionTrns [testFlatConReg1,testFlatConReg2]) 
-  print (crossInms testFlat1Combine)
-  print (mrgInmsCP (crossInms testFlat1Combine))
+  print (show $ globalise testFlat1Input)
+  print (show $ localise testFlat1Input)
+  print "flattening test"
+  print (show (flatten testFlat1Input))
   let sd = testFlat1Input
   mainWith (drawDiagram sd)
 
--- src/Flatten.hs
+++ src/Flatten.hs
@@ -1,58 +1,91 @@
 {-# LANGUAGE NamedFieldPuns            #-}
 
 module Flatten (
-  crossInms
-  ,unionTrns
-  ,mrgInmsCP
+    getLabelStr
+   ,flatten
 ) where
 import Datatype (UMLStateDiagram
                 ,StateDiagram(..)
-                ,Connection (..))
+                ,Connection (..)
+                ,globalise)
 
-{-
+
 flatten :: UMLStateDiagram -> UMLStateDiagram
-flatten (StateDiagram { substate = (x:xs)}) = let
-                                              -- apply outermost transitions
-                                              in
-                                              
-flatten _ = error "invalid UMLStateDiagram"
+flatten d = StateDiagram (bridge (toImsCp' (globalise d))) 0 "" [] [0]
+            -- currently flattens states one layer too high
+            -- transitions are not taken care of wip yet because id remap is required
+            -- which was the reason to introduce the ImsCp data type
 
-flatten' :: UMLStateDiagram
-flatten' sd@(StateDiagram { substate = (x:xs)}) | sd `withOnlyInms` = 
-flatten' (CombineDiagram {}) = error "invalid UMLStateDiagram"
-flatten' (EndState {}) = error "invalid UMLStateDiagram"
-flatten' (Joint {}) = error "invalid UMLStateDiagram"
-flatten' (History {}) = error "invalid UMLStateDiagram"
-flatten' (InnerMostState {}) = error "invalid UMLStateDiagram"
--}
+withOnlyIms :: UMLStateDiagram -> Bool
+withOnlyIms (StateDiagram {substate})
+    =  and (True:map withOnlyIms' substate)
+withOnlyIms _ = False
 
-{- arbitrarily long cp's of states -}
-data InmsCP = InmsCP UMLStateDiagram (Maybe InmsCP)
+withOnlyIms' :: UMLStateDiagram -> Bool
+withOnlyIms' (InnerMostState {}) = True
+withOnlyIms' _ = False
+
+bridge :: [ImsCp] -> [UMLStateDiagram]
+bridge imss = let
+              in
+              [bridge' x | x <- imss]
+
+bridge' :: ImsCp -> UMLStateDiagram
+bridge' imscp = InnerMostState 0 (getLabelStr imscp) ""
+
+toImsCp' :: UMLStateDiagram -> [ImsCp]
+toImsCp' sd@(StateDiagram {label,name,substate})
+   | withOnlyIms sd =
+      let
+      rt = InnerMostState label name ""
+      in
+      [imsCpCtr [rt,x]|x <- substate]
+   | otherwise = crsImsCps (map toImsCp' substate)
+toImsCp' (CombineDiagram {label, substate})
+   =
+   let imss = crsImsCps (map toImsCp' substate)
+       rt = InnerMostState label "" ""
+   in
+   [ImsCp rt (Just x) | x <- imss]
+toImsCp' ims@(InnerMostState {}) = [ImsCp ims Nothing]
+toImsCp' _ = []
+
+data ImsCp = ImsCp UMLStateDiagram (Maybe ImsCp)
      deriving (Eq)
 
-instance Show InmsCP where
-   show (InmsCP (InnerMostState {name}) (Just inmsCP)) = name ++ " " ++ show inmsCP
-   show (InmsCP (InnerMostState {name}) Nothing) = name ++ "\n"
+instance Show ImsCp where
+   show (ImsCp (InnerMostState {name, label}) (Just inmsCP))
+    = name ++ " " ++ (show label) ++ " " ++ show inmsCP
+   show (ImsCp (InnerMostState {name, label}) Nothing)
+    = name ++ " " ++ (show label) ++ " " ++ "\n"
    show _ = "bad inmsCP instance"
 
-{- form the cross product of substate regions.
-   should only be applied when substates only consist of InnerMostStates
-   hence the name Inms := InnerMostStates
--}
-crossInms :: [[UMLStateDiagram]] -> [InmsCP]
-crossInms [] = []
-crossInms [x] = [InmsCP y Nothing | y <- x ]
-crossInms (x:xs) = [InmsCP y (Just z) | y <- x, z <- crossInms xs ]
+imsCpCtr :: [UMLStateDiagram] -> ImsCp
+imsCpCtr [] = error "must provide states"
+imsCpCtr [x] = ImsCp x Nothing
+imsCpCtr (x:xs) = ImsCp x (Just (imsCpCtr xs))
 
-{- unify the sets of transitions of two regions -}
-unionTrns :: [UMLStateDiagram] -> [Connection]
-unionTrns [] = []
-unionTrns (x:xs) = case x of
-                     (StateDiagram {connection}) -> connection ++ unionTrns xs
-                     _ -> []
+getImss :: ImsCp -> [UMLStateDiagram]
+getImss (ImsCp x Nothing) = [x]
+getImss (ImsCp x (Just y)) = x:getImss y
 
-{- simple getter for labels on various data types
-   to avoid having to pattern match when its cumbersome -}
+crsImsCps :: [[ImsCp]] -> [ImsCp]
+crsImsCps [x] = x
+crsImsCps (x:y:xs) = crsImsCps (crsImsCps'' x y:xs)
+crsImsCps _ = error "shouldnt happen"
+
+crsImsCps' :: [UMLStateDiagram] -> ImsCp -> ImsCp
+crsImsCps' [] y = y
+crsImsCps' [x] y = ImsCp x (Just y)
+crsImsCps' (x:xs) y = ImsCp x (Just (crsImsCps' xs y))
+
+crsImsCps'' :: [ImsCp] -> [ImsCp] -> [ImsCp]
+crsImsCps'' [] ys = ys
+crsImsCps'' xs [] = xs
+crsImsCps'' xs ys = [crsImsCps' (getImss x) y | x <- xs, y <- ys]
+
+
+
 class Labeled a where
    getLabelStr :: a -> String
 
@@ -64,31 +97,10 @@ instance Labeled UMLStateDiagram where
     getLabelStr (History {}) = error "not defined"
     getLabelStr (InnerMostState {name}) = name
 
-instance Labeled InmsCP where
-    getLabelStr (InmsCP x Nothing) = getLabelStr x
-    getLabelStr (InmsCP x (Just s)) = getLabelStr x ++ ", " ++ getLabelStr s
+instance Labeled ImsCp where
+    getLabelStr (ImsCp x Nothing) = getLabelStr x
+    getLabelStr (ImsCp x (Just s)) = getLabelStr x ++ ", " ++ getLabelStr s
 
-{- merge crossproducts of states that were obtained into
-   a visually representable form, redistributes ids -}
-mrgInmsCP :: [InmsCP] -> UMLStateDiagram
-mrgInmsCP xs = StateDiagram (mrgInmsCP' xs 1) 0 "" [] []
+instance Labeled Connection where
+   getLabelStr (Connection {pointFrom, pointTo, transition}) = transition ++ (show pointFrom) ++ (show pointTo)
 
-mrgInmsCP' :: [InmsCP] -> Int -> [UMLStateDiagram]
-mrgInmsCP' [] _ = []
-mrgInmsCP' (x:xs) y = let i = InnerMostState y (getLabelStr x) ""
-                      in
-                      i:mrgInmsCP' xs (y+1)
-
-
-{-
-mapUTrnsToInmsCPs :: [Connection] -> [InmsCP] -> [InmsCP]
-mapUTrnsToInmsCPs trs ims = 
--}
-
-{-
-   todo impl: create crossproducts of regions recursively from bottom up
-              remap their transitions to their new states
-              once at top level remap outermost transitions into cped states
-   might require another type for storing CPS with ID that has not been folded
-   into a visualizable state yet
--}
