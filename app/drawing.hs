14f1e7292bd05075eb90ebdff086ea12caa47622
sjselest <sergei.lestsenski@stud.uni-due.de>
2023-05-15 17:06:08 +0200

    ' fix thats been in the wrong place
    and further preparation for restoring transitions

--- app/drawing.hs
+++ app/drawing.hs
@@ -3,15 +3,13 @@ import Layout (drawDiagram)
 import Example (testFlat1Input)
 import Datatype ( localise
                   ,globalise )
-import Flatten (flatten,
-                mergeStates,
-                truncateUpperLabels)
+import Flatten (flatten)
 
 main :: IO ()
 main = do
   print (show $ globalise testFlat1Input)
   print (show $ localise testFlat1Input)
   print "flatten test"
-  print (show (truncateUpperLabels( mergeStates (flatten testFlat1Input))))
+  print (show (flatten testFlat1Input))
   let sd = {- testFlat1Input -} testFlat1Input
   mainWith (drawDiagram sd)
--- src/Flatten.hs
+++ src/Flatten.hs
@@ -4,15 +4,20 @@ module Flatten (
    flatten
    ,mergeStates
    ,truncateUpperLabels
+   ,redistributeLabels
 ) where
 import Datatype (UMLStateDiagram
                 ,StateDiagram(..)
+                ,globalise
+                --,Connection(..)
+                )
 
-                ,globalise)
 
-
-flatten :: UMLStateDiagram -> [Flattened]
-flatten d = flatten' (globalise d)
+flatten :: UMLStateDiagram -> [([[Int]], UMLStateDiagram)]
+flatten d = let
+            g = globalise d
+            in
+            redistributeLabels (mergeStates (truncateUpperLabels (flatten' g)))
 
 withOnlyInnerMost :: UMLStateDiagram -> Bool
 withOnlyInnerMost (StateDiagram {substate})
@@ -35,26 +40,28 @@ getLabel (StateDiagram {label}) = label
 getLabel (CombineDiagram {label}) = label
 getLabel _ = 0
 
-mergeStates :: [(a, [UMLStateDiagram])] -> [(a, UMLStateDiagram)]
+mergeStates :: [([[Int]], [UMLStateDiagram])] -> [([[Int]], UMLStateDiagram)]
 mergeStates flattened = [(xs, mergeStates' ys)|(xs,ys)<-flattened]
 
-mergeStates' :: [UMLStateDiagram] -> UMLStateDiagram
+mergeStates' :: [UMLStateDiagram] -> UMLStateDiagram -- op order is important! type change
 mergeStates' innerStates = InnerMostState 0 (concat [name ++ ", "| InnerMostState{name}<-innerStates,name /= ""]) ""
 
-truncateUpperLabels :: [([[a]], b)] -> [([[a]], b)]
+truncateUpperLabels :: [Flattened] -> [Flattened]
 truncateUpperLabels flattened = [([tail x|x<-xs],ys)|(xs,ys)<-flattened]
 
-{-
-  next steps; redistribute fresh labels onto flattened states
-  now use [[Int]] ~ label as a lookup table to remap the transitions correctly
--}
+redistributeLabels :: [([[Int]], UMLStateDiagram)] -> [([[Int]], UMLStateDiagram)]
+redistributeLabels xs = redistributeLabels' xs 1
+
+redistributeLabels' :: [([[Int]], UMLStateDiagram)] -> Int -> [([[Int]], UMLStateDiagram)]
+redistributeLabels' ((x,y):xs) i = (x,(y {label = i})) : redistributeLabels' xs (i + 1)
+redistributeLabels' _ _ = []
 
 flatten' :: UMLStateDiagram -> [Flattened]
 flatten' parent@(StateDiagram {substate = substates})
     | withOnlyInnerMost parent = inheritFrom substates parent
-    | otherwise = inheritFrom' (concatMap flatten substates) parent
+    | otherwise = inheritFrom' (concatMap flatten' substates) parent
 flatten' parent@(CombineDiagram{substate = substates})
-    = inheritFrom' (cartesianProduct $ map flatten substates) parent
+    = inheritFrom' (cartesianProduct $ map flatten' substates) parent
 flatten' innerMost@(InnerMostState {label}) = [([[label]],[innerMost])]
 flatten' _ = []
 
@@ -76,3 +83,25 @@ cartesianProduct :: [[Flattened]] -> [Flattened]
 cartesianProduct [] = []
 cartesianProduct [x] = x
 cartesianProduct (x:y:xs) = cartesianProduct (cartesianProduct' x y : xs)
+
+{-
+  next steps; redistribute fresh labels onto flattened states
+  now use [[Int]] ~ label as a lookup table to remap the transitions correctly
+-}
+
+{-
+  identify 1:1, 1:N (fork), N:1 (join) and restore
+-}
+
+{-
+extractOneToOne :: UMLStateDiagram -> [Connection]
+extractOneToOne (StateDiagram {connection})
+    = [ c| c@(Connection{ pointFrom, pointTo}) <- connection
+    , length pointFrom == 1
+    , length pointTo == 1 ]
+extractOneToOne _ = []
+
+
+restoreOneToOne :: [([[Int]], UMLStateDiagram)] -> [Connection] -> [Connection]
+restoreOneToOne flat cs = [  |fl <- flat, c@(Connection{pointFrom,pointTo}) <- cs, pointFrom  ]
+-}
