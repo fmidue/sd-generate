873c2365079b99260382f70425288ded83edcedf
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2023-10-23 18:50:34 +0200

    dirtily making test suite compile again

--- test/PlantUMLDiagramsSpec.hs
+++ test/PlantUMLDiagramsSpec.hs
@@ -3,12 +3,14 @@ module PlantUMLDiagramsSpec (spec) where
 import Common                           (withUnitTests)
 import PlantUMLDiagrams                 (renderAll)
 import Datatype                         (umlStateDiagram)
+import Style (Styling(..))
+import System.IO.Unsafe (unsafePerformIO)
 
 import Test.Hspec
 
 spec :: Spec
 spec =
-  withUnitTests "renderAll" does dir "txt" $ shouldBe . renderAll . umlStateDiagram . read
+  withUnitTests "renderAll" does dir "txt" $ shouldBe . unsafePerformIO . renderAll Unstyled . umlStateDiagram . read
   where
     does = "generates expected PlantUML code"
     dir = "test/unit/PlantUMLDiagrams"
