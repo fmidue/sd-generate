1150abf19e252d8af28eeef824ee04fcbf92e473
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2023-11-29 21:06:02 +0100

    add quantitative control over entities not previously covered

--- src/Modelling/StateDiagram/Config.hs
+++ src/Modelling/StateDiagram/Config.hs
@@ -32,10 +32,13 @@ data ChartLimits
                 , regions :: (Int,Int)
                 , normalStates :: (Int,Int)
                 , componentNames :: (Int,Int)
+                , triggerNames :: (Int,Int)
+                , startNodes :: (Int,Int)
                 , endNodes :: (Int,Int)
                 , forkNodes :: (Int,Int)
                 , joinNodes :: (Int,Int)
-                , historyNodes :: (Int,Int)
+                , shallowHistoryNodes :: (Int,Int)
+                , deepHistoryNodes :: (Int,Int)
                 , flows :: (Int,Int)
                 , protoFlows :: (Int,Int)
                 , totalNodes :: (Int,Int)
@@ -74,10 +77,13 @@ defaultSDConfigScenario1
                              , regions = (0,0)
                              , normalStates = (7,7)
                              , componentNames = (0,10)
+                             , triggerNames = (0,10)
+                             , startNodes = (0,0)
                              , endNodes = (0,0)
                              , forkNodes = (0,0)
                              , joinNodes = (0,0)
-                             , historyNodes = (0,0)
+                             , shallowHistoryNodes = (0,0)
+                             , deepHistoryNodes = (0,0)
                              , flows = (11,11)
                              , protoFlows = (17,17)
                              , totalNodes = (10,10)
@@ -99,11 +105,14 @@ defaultSDConfigScenario2
                              , hierarchicalStates = (1,1)
                              , regions = (2,2)
                              , normalStates = (8,8)
+                             , startNodes = (1,1)
                              , endNodes = (1,1)
                              , componentNames = (9,9)
+                             , triggerNames = (9,9)
                              , forkNodes = (0,0)
                              , joinNodes = (0,0)
-                             , historyNodes = (0,0)
+                             , shallowHistoryNodes = (0,0)
+                             , deepHistoryNodes = (0,0)
                              , flows = (10,10)
                              , protoFlows = (10,10)
                              , totalNodes = (12,12)
@@ -126,10 +135,13 @@ defaultSDConfigScenario3
                              , regions = (0,0)
                              , normalStates = (8,8)
                              , componentNames = (11,11)
+                             , triggerNames = (11,11)
+                             , startNodes = (0,0)
                              , endNodes = (0,0)
                              , forkNodes = (0,0)
                              , joinNodes = (0,0)
-                             , historyNodes = (2,2)
+                             , shallowHistoryNodes = (1,1)
+                             , deepHistoryNodes = (1,1)
                              , flows = (10,10)
                              , protoFlows = (10,10)
                              , totalNodes = (11,11)
@@ -146,10 +158,13 @@ checkSDConfig SDConfig { scope
                                                    , regions
                                                    , normalStates
                                                    , componentNames
+                                                   , triggerNames
+                                                   , startNodes
                                                    , endNodes
                                                    , forkNodes
                                                    , joinNodes
-                                                   , historyNodes
+                                                   , shallowHistoryNodes
+                                                   , deepHistoryNodes
                                                    , flows
                                                    , protoFlows
                                                    , totalNodes
@@ -163,20 +178,27 @@ checkSDConfig SDConfig { scope
   | uncurry (>) regions = Just "minRegions must be less than or equal to maxRegions"
   | uncurry (>) normalStates = Just "minNormalStates must be less than or equal to maxNormalStates"
   | fst componentNames < 0 = Just "componentNames must be greater than or equal to 0"
+  | fst triggerNames < 0 = Just "triggerNames must be greater than or equal to 0"
+  | uncurry (>) startNodes = Just "minStartNodes must be less than or equal to maxStartNodes"
   | uncurry (>) endNodes = Just "minEndNodes must be less than or equal to maxEndNodes"
   | uncurry (>) forkNodes = Just "minForkNodes must be less than or equal to maxForkNodes"
   | uncurry (>) joinNodes = Just "minJoinNodes must be less than or equal to maxJoinNodes"
-  | uncurry (>) historyNodes = Just "minHistoryNodes must be less than or equal to maxHistoryNodes"
+  | uncurry (>) shallowHistoryNodes = Just "minShallowHistoryNodes must be less than or equal to maxShallowHistoryNodes"
+  | uncurry (>) deepHistoryNodes = Just "minDeepHistoryNodes must be less than or equal to maxDeepHistoryNodes"
   | uncurry (>) flows = Just "minFlows must be less than or equal to maxFlows"
   | fst protoFlows < 0 = Just "protoFlows must be greater than or equal to 0"
+  | fst startNodes < 1 = Just "you likely want to have at least one startNode in the chart"
   | fst endNodes < 1 = Just "you likely want to have at least one EndNode in the chart"
   | snd regions > 6 = Just "you likely want to have less than 7 Regions in the chart"
   | snd normalStates > 15 = Just "you likely want to have less than 16 NormalStates in the chart"
   | snd componentNames > 16 = Just "you likely want to have less than 16 ComponentNames in the chart"
+  | snd triggerNames > 16 = Just "you likely want to have less than 16 TriggerNames in the chart"
+  | snd startNodes > 4 = Just "you likely want to have less than 4 StartNodes in the chart"
   | snd endNodes > 4 = Just "you likely want to have less than 4 EndNodes in the chart"
   | snd forkNodes > 3 = Just "you likely want to have less than 4 ForkNodes in the chart"
   | snd joinNodes > 3 = Just "you likely want to have less than 4 JoinNodes in the chart"
-  | snd historyNodes > 5 = Just "you likely want to have less than 6 HistoryNodes in the chart"
+  | snd shallowHistoryNodes > 5 = Just "you likely want to have less than 6 ShallowHistoryNodes in the chart"
+  | snd deepHistoryNodes > 5 = Just "you likely want to have less than 6 DeepHistoryNodes in the chart"
   | distinctNormalStateNames && not enforceNormalStateNames = Just "you cannot enforce distinct normal state names without enforcing normal state names"
   | uncurry (>) totalNodes = Just "you likely want to have at least 4 Nodes in the chart"
   | otherwise = Nothing
@@ -195,10 +217,13 @@ sdConfigToAlloy  SDConfig { scope
                                                       , regions
                                                       , normalStates
                                                       , componentNames
+                                                      , triggerNames
+                                                      , startNodes
                                                       , endNodes
                                                       , forkNodes
                                                       , joinNodes
-                                                      , historyNodes
+                                                      , shallowHistoryNodes
+                                                      , deepHistoryNodes
                                                       , flows
                                                       , protoFlows
                                                       , totalNodes
@@ -232,10 +257,13 @@ pred scenarioConfig #{oB}
   #{if noNestedEndNodes then "EndNodes not in allContainedNodes" else ""}
   #{bounded regions "Regions"}
   #{bounded hierarchicalStates "HierarchicalStates"}
+  #{bounded startNodes "StartNodes"}
   #{bounded endNodes "EndNodes"}
-  #{bounded historyNodes "HistoryNodes"}
+  #{bounded shallowHistoryNodes "ShallowHistoryNodes"}
+  #{bounded deepHistoryNodes "DeepHistoryNodes"}
   #{bounded normalStates "NormalStates"}
   #{bounded componentNames "ComponentNames"}
+  #{bounded triggerNames "TriggerNames"}
   #{bounded forkNodes "ForkNodes"}
   #{bounded joinNodes "JoinNodes"}
   #{bounded flows "Flows"}
@@ -248,13 +276,16 @@ pred scenarioConfig #{oB}
 #{cB}
 
 run scenarioConfig for #{scope} but #{bitwidth} Int,
+#{caseExact startNodes "StartNodes"},
 #{caseExact endNodes "EndNodes"},
 #{caseExact normalStates "NormalStates"},
-#{caseExact historyNodes "HistoryNodes"},
+#{caseExact shallowHistoryNodes "ShallowHistoryNodes"},
+#{caseExact deepHistoryNodes "DeepHistoryNodes"},
 #{caseExact hierarchicalStates "HierarchicalStates"},
 #{caseExact flows "Flows"},
 #{show (snd protoFlows) ++ " ProtoFlows"},
 #{caseExact componentNames "ComponentNames"},
+#{caseExact triggerNames "TriggerNames"},
 #{caseExact regionStates "RegionsStates"},
 #{caseExact forkNodes "ForkNodes"},
 #{caseExact joinNodes "JoinNodes"},
