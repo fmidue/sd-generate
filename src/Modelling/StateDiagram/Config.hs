30ff6a7515565e6220205cf12124cd8de18b92cd
sjselest <sergei.lestsenski@stud.uni-due.de>
2023-12-07 17:41:59 +0100

    Update Config.hs

--- src/Modelling/StateDiagram/Config.hs
+++ src/Modelling/StateDiagram/Config.hs
@@ -70,7 +70,7 @@ defaultSDConfigScenario1
   = SDConfig { bitwidth = 6
              , enforceNormalStateNames = True
              , distinctNormalStateNames = True
-             , preventEmptyTriggersFromStates = False
+             , preventEmptyTriggersFromStates = True
              , distinctTriggerNames = False
              , preventNestedEndNodes = False
              , preventMultiEdgesInOriginalDiagram = Nothing
