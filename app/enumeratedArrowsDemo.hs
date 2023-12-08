1a854a8361df50d65405ba77276216b7c982030b
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2023-12-08 22:18:18 +0100

    indent and simplify

--- app/enumeratedArrowsDemo.hs
+++ app/enumeratedArrowsDemo.hs
@@ -8,7 +8,7 @@ import Control.Monad.Output (
   )
 import Control.Monad.Output.Generic     (($>>=))
 import Control.Monad.Output.Debug       (testTask)
-
+import Data.Functor                     (($>))
 import Data.Time.Clock.POSIX(getPOSIXTime)
 import System.Directory(createDirectoryIfMissing
                        ,renameFile)
@@ -30,49 +30,49 @@ main :: IO ()
 main = do
   t <- getSeed
   testTask English (generate t) (describe t) partial full submission
- where
-  getSeed :: IO Int
-  getSeed = round <$> getPOSIXTime
-  generate :: Int -> IO EnumArrowsInstance
-  generate timestamp = do
-    hSetBuffering stdout NoBuffering
-    -- check the task configuration
-    forM_ (checkEnumArrowsConfig defaultEnumArrowsConfig) error
-    putStrLn "configuration looking good"
-    createDirectoryIfMissing True ("./session_temp/enumArrows"::FilePath)
-    putStrLn $ "Seed: " ++ show timestamp
-    -- initialize Alloy and instance selector
+  where
+    getSeed :: IO Int
+    getSeed = round <$> getPOSIXTime
+    generate :: Int -> IO EnumArrowsInstance
+    generate timestamp = do
+      hSetBuffering stdout NoBuffering
+      -- check the task configuration
+      forM_ (checkEnumArrowsConfig defaultEnumArrowsConfig) error
+      putStrLn "configuration looking good"
+      createDirectoryIfMissing True ("./session_temp/enumArrows"::FilePath)
+      putStrLn $ "Seed: " ++ show timestamp
+      -- initialize Alloy and instance selector
 
-    -- and pick a concrete instance, and optionally randomise triggers and names
-    task <- enumArrows defaultEnumArrowsConfig timestamp >>= randomise >>= randomiseLayout
-    -- visualize task
-    pure task
-  describe :: Int -> EnumArrowsInstance -> LangM (ReportT (IO ()) IO)
-  describe timestamp task = do
-   enumArrowsTask ("./session_temp/enumArrows"::FilePath) task
-   $>>= \x -> (*> pure x) $ pure $ do
-    print task
-    putStrLn ("\n" ++ "cheat solution: " ++ show (concatMap (uncurry zip) $ taskSolution task))
-    -- user response (task assignment -> solution submission)
+      -- and pick a concrete instance, and optionally randomise triggers and names
+      enumArrows defaultEnumArrowsConfig timestamp
+        >>= randomise
+        >>= randomiseLayout
+      -- visualize task
+    describe :: Int -> EnumArrowsInstance -> LangM (ReportT (IO ()) IO)
+    describe timestamp task =
+      enumArrowsTask ("./session_temp/enumArrows"::FilePath) task
+      $>>= \x -> (*> pure x) $ pure $ do
+        print task
+        putStrLn ("\n" ++ "cheat solution: " ++ show (concatMap (uncurry zip) $ taskSolution task))
+        -- user response (task assignment -> solution submission)
 
-    -- rename the session files and store the solution
-    putStrLn ("the .svg files have been renamed to include the seed: " ++ show timestamp)
-    renameFile ("./session_temp/enumArrows" ++ "/plainDiagram.svg")
-               ("./session_temp/enumArrows/" ++ show timestamp ++ "_plainDiagram.svg")
-    renameFile ("./session_temp/enumArrows" ++ "/flattenedDiagram.svg")
-               ("./session_temp/enumArrows/" ++ show timestamp ++ "_flattenedDiagram.svg")
-    writeFile ("./session_temp/enumArrows/" ++ show timestamp ++ "_solution.txt") (show (taskSolution task))
-    writeFile ("./session_temp/enumArrows/" ++ show timestamp ++ "_generatorConfig.txt") (show defaultEnumArrowsConfig)
-  submission = do
-    -- user submission
-    sub <- fmap read getLine
-    pure sub
-  partial task sub = do
-    -- user submission syntax checking
-    enumArrowsSyntax task sub
-  full :: EnumArrowsInstance -> [(String, String)] -> Rated (ReportT (IO ()) IO)
-  full task sub =
-    -- task submission evaluation function
-    enumArrowsEvaluation task sub
-    -- extended submission feedback
-    $>>= \points -> enumArrowsFeedback task sub *> pure points
+        -- rename the session files and store the solution
+        putStrLn ("the .svg files have been renamed to include the seed: " ++ show timestamp)
+        renameFile ("./session_temp/enumArrows" ++ "/plainDiagram.svg")
+                   ("./session_temp/enumArrows/" ++ show timestamp ++ "_plainDiagram.svg")
+        renameFile ("./session_temp/enumArrows" ++ "/flattenedDiagram.svg")
+                   ("./session_temp/enumArrows/" ++ show timestamp ++ "_flattenedDiagram.svg")
+        writeFile ("./session_temp/enumArrows/" ++ show timestamp ++ "_solution.txt") (show (taskSolution task))
+        writeFile ("./session_temp/enumArrows/" ++ show timestamp ++ "_generatorConfig.txt") (show defaultEnumArrowsConfig)
+    submission =
+      -- user submission
+      fmap read getLine
+    partial =
+      -- user submission syntax checking
+      enumArrowsSyntax
+    full :: EnumArrowsInstance -> [(String, String)] -> Rated (ReportT (IO ()) IO)
+    full task sub =
+      -- task submission evaluation function
+      enumArrowsEvaluation task sub
+      -- extended submission feedback
+      $>>= (enumArrowsFeedback task sub $>)
